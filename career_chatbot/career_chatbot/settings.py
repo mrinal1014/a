0214015b86c989429d5f85814db98ff277c47a94
mrinal1014 <131162988+mrinal1014@users.noreply.github.com>
2025-04-23 04:22:28 +0530

    Update settings.py

--- career_chatbot/career_chatbot/settings.py
+++ career_chatbot/career_chatbot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,20 +76,17 @@ WSGI_APPLICATION = 'career_chatbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
+
+
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 DATABASES = {
-    'default': env.db()  # Reads from DATABASE_URL
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
